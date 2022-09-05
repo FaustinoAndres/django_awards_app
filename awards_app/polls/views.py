5629805ba42988a34e718c663402929a77273ddb
Faustino Correa <faustino.correa.m@gmail.com>
2022-09-05 15:40:20 -0400

    creando templates

--- awards_app/polls/views.py
+++ awards_app/polls/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Estás en el Home")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {'latest_question_list': latest_question_list})
 
 
 def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
     return HttpResponse(f"Estás viendo la pregunta número {question_id}")
 
 
