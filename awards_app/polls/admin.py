1d9651fe99463a003776b97510229999d13e5b5e
Faustino Correa <faustino.correa.m@gmail.com>
2022-09-08 10:54:53 -0400

    implementando django polymorphic

--- awards_app/polls/admin.py
+++ awards_app/polls/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from polls.models import Question
-from .models import Question
+from polls.models import *
 
 # Register your models here.
 
 admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(LongChoice)
+admin.site.register(ChoicePoly)
+admin.site.register(LongChoicePoly)
--- awards_app/polls/models.py
+++ awards_app/polls/models.py
@@ -1,7 +1,10 @@
 import datetime
+from polymorphic.models import PolymorphicModel
 
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -14,10 +17,26 @@ class Question(models.Model):
         return self.pub_date.date() >= timezone.now() - datetime.timedelta(days=1)
 
 
-class Choice(models.Model):
+class Choice(PolymorphicModel):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return self.choice_text
+
+
+class LongChoice(Choice):
+    long_choice_text = models.CharField(max_length=1000)
+
+
+class ChoicePoly(PolymorphicModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
+
+class LongChoicePoly(ChoicePoly):
+    long_choice_text = models.CharField(max_length=1000)
